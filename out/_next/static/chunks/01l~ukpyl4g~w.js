197c31c45540a12f3076903905396968375909a6
SirautiToiro <sirautitoiro@gmail.com>
2026-07-18 18:11:34 +0900

    build as static.

---
+++ out/_next/static/chunks/01l~ukpyl4g~w.js
@@ -0,0 +1,1 @@
+(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,25834,o=>{o.v(t=>Promise.all(["static/chunks/14opfn~7mfuub.js"].map(t=>o.l(t))).then(()=>t(18657)))}]);
